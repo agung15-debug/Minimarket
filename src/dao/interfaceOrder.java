b08cc02ff3f2e8f130a138882285ae2694c6dc38
Visen <82668173+Viseenn@users.noreply.github.com>
2022-06-02 20:44:51 +0700

    Update interfaceOrder.java

--- src/dao/interfaceOrder.java
+++ src/dao/interfaceOrder.java
@@ -1,21 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package dao;
 
 import model.Order;
-import model.Pegawai;
 import java.util.List;
 
 public interface interfaceOrder {
-    public void insert(Pegawai pegawai);
+    public void insert(Order order);
 
     public void update(Order order);
 
     public void delete(int id);
 
     public List<Order> getData();
-
-    //public void insert(Order order, int idPegawai);
 }
